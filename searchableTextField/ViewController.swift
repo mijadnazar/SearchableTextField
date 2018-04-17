bea0f57c159be4ae60f0aabe99c7819e17279be4
mehdi ijadnazar <ijadnazar.mahdi@gmail.com>
2018-04-17 18:37:45 +0430

    Initial Commit

---
+++ searchableTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  searchableTextField
+//
+//  Created by mobile mac mini on 4/17/18.
+//  Copyright © 2018 Mehdi Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
